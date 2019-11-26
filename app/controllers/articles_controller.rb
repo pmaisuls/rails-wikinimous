8358e662eb49b657b35157705f0960b3dd02acc7
Pablo Maisuls <pablo_maisuls@live.com>
2019-11-26 16:46:09 +0100

    first version of Rails Wikinimous

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(task_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to @articles_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+end
+
